ab7ae88e6e10ac683d309353c56df2bd30b747c0
woojulee24 <bigtree123433@gmail.com>
2024-10-17 12:41:42 +0000

    1. kitti2, 2.wandb, 3. vis, 4. debug model

---
+++ PureACL/pixlib/models/two_view_refiner_debug.py
@@ -0,0 +1,356 @@
+"""
+The top-level model of training-time sidfm.
+Encapsulates the feature extraction, pose optimization, loss and metrics.
+"""
+import torch
+from torch.nn import functional as nnF
+import logging
+from copy import deepcopy
+import omegaconf
+import itertools
+
+from PureACL.pixlib.models.base_model import BaseModel
+from PureACL.pixlib.models import get_model
+from PureACL.pixlib.models.utils import masked_mean, merge_confidence_map, extract_keypoints, camera_to_onground
+from PureACL.pixlib.geometry.losses import scaled_barron
+
+
+logger = logging.getLogger(__name__)
+
+pose_loss = True
+
+def get_weight_from_reproloss(err):
+    # the reprojection loss is from 0 to 16.67 ,tensor[B]
+    err = err.detach()
+    weight = torch.ones_like(err)*err
+    weight[err < 10.] = 0
+    weight = torch.clamp(weight, min=0., max=50.)
+    return weight
+
+class TwoViewRefiner(BaseModel):
+    default_conf = {
+        'extractor': {
+            'name': 'unet_debug', #'s2dnet',
+        },
+        'optimizer': {
+            'name': 'learned_optimizer', #'basic_optimizer',
+        },
+        'duplicate_optimizer_per_scale': True,
+        'success_thresh': 3,
+        'clamp_error': 50,
+        'normalize_features': True,
+        'normalize_dt': True,
+        'debug': False,
+        # deprecated entries
+        'init_target_offset': None,
+
+        'grd_height': 1.65,
+    }
+    required_data_keys = {
+        'ref': ['image', 'camera', 'T_w2cam'],
+        'query': ['image', 'camera', 'T_w2cam'],
+    }
+    strict_conf = False  # need to pass new confs to children models
+
+    def _init(self, conf):
+        self.extractor = get_model(conf.extractor.name)(conf.extractor)
+        assert hasattr(self.extractor, 'scales')
+
+        Opt = get_model(conf.optimizer.name)
+        if conf.duplicate_optimizer_per_scale:
+            oconfs = [deepcopy(conf.optimizer) for _ in self.extractor.scales]
+            feature_dim = self.extractor.conf.output_dim
+            if not isinstance(feature_dim, int):
+                for d, oconf in zip(feature_dim, oconfs):
+                    with omegaconf.read_write(oconf):
+                        with omegaconf.open_dict(oconf):
+                            oconf.feature_dim = d
+            self.optimizer = torch.nn.ModuleList([Opt(c) for c in oconfs])
+        else:
+            self.optimizer = Opt(conf.optimizer)
+
+        if conf.init_target_offset is not None:
+            raise ValueError('This entry has been deprecated. Please instead '
+                             'use the `init_pose` config of the dataloader.')
+        self.grd_height = conf.grd_height
+
+    def _forward(self, data):
+        def process_siamese(data_i, data_type):
+            if data_type == 'ref':
+                data_i['type'] = 'sat'
+                data_i['q2r'] = data['T_q2r_init']
+                data_i['q2r_gt'] = data['T_q2r_gt']
+                data_i['query'] = data['query']
+            else:
+                data_i['type'] = 'grd'
+                data_i['q2r'] = data['T_q2r_init']
+                data_i['q2r_gt'] = data['T_q2r_gt']
+                data_i['normal'] = data['normal']
+                data_i['ref'] = data['ref']
+            pred_i = self.extractor(data_i)
+            pred_i['camera_pyr'] = [data_i['camera'].scale(1 / s)
+                                    for s in self.extractor.scales]
+            return pred_i
+        # start_time = time.time()
+        if 'query_3' in data.keys():
+            pred = {i: process_siamese(data[i], i) for i in ['ref', 'query', 'query_1', 'query_2','query_3']}
+        elif 'query_1' in data.keys():
+            pred = {i: process_siamese(data[i], i) for i in ['ref', 'query', 'query_1']}
+        else:
+            pred = {i: process_siamese(data[i], i) for i in ['ref', 'query']}
+        # after_time = time.time()
+        # print('duration:',after_time-start_time)
+
+        confidence_count = 2
+        if pred['ref']['confidences'][0].size(1) == 1:
+            confidence_count = 1
+
+        # find ground key points from confidence map. top from each grd_img
+        if 'query_3' in data.keys():
+            query_list = ['query', 'query_1', 'query_2', 'query_3']
+        elif 'query_1' in data.keys():
+            query_list = ['query', 'query_1']
+        else:
+            query_list = ['query']
+        for q in query_list:
+            # find 2d key points from grd confidence map
+            grd_key_confidence = merge_confidence_map(pred[q]['confidences'],confidence_count) #[B,H,W]
+            p2d_grd_key = extract_keypoints(grd_key_confidence, start_ratio = data[q]['camera'].c[0,1]/data[q]['camera'].size[0,1]+0.05) #data['grd_ratio'][0])
+
+            # turn grd key points from 2d to 3d, assume points are on ground
+            p2d_c_key = data[q]['camera'].image2world(p2d_grd_key) # 2D->3D scale unknown
+            p3d_grd_key = camera_to_onground(p2d_c_key, data[q]['T_w2cam'], data[q]['camera_h'], data['normal'])
+            if q == 'query':
+                p3D_query = p3d_grd_key
+            else:
+                p3D_query = torch.cat([p3D_query, p3d_grd_key], dim=1)
+        pred['query']['grd_key_3d'] = p3D_query
+
+
+        T_init = data['T_q2r_init']
+
+        pred['T_q2r_init'] = []
+        pred['T_q2r_opt'] = []
+        pred['T_q2r_opt_list'] = []
+        pred['pose_loss'] = []
+        for i in reversed(range(len(self.extractor.scales))):
+            F_ref = pred['ref']['feature_maps'][i]
+            cam_ref = pred['ref']['camera_pyr'][i]
+
+            if self.conf.duplicate_optimizer_per_scale:
+                opt = self.optimizer[i]
+            else:
+                opt = self.optimizer
+
+            if 'query_3' in data.keys():
+                querys = ['query', 'query_1', 'query_2', 'query_3']
+            elif 'query_1' in data.keys():
+                querys = ['query', 'query_1']
+            else:
+                querys = ['query']
+
+            W_q = None
+            F_q = None
+            mask = None
+            for q in querys:
+                F_q_cur = pred[q]['feature_maps'][i]
+                cam_q = pred[q]['camera_pyr'][i]
+
+                p2D_query, visible = cam_q.world2image(data[q]['T_w2cam'] * p3D_query)
+                F_q_cur, mask_cur, _ = opt.interpolator(F_q_cur, p2D_query)
+                mask_cur &= visible
+
+                W_q_cur = pred[q]['confidences'][i]
+                W_q_cur, _, _ = opt.interpolator(W_q_cur, p2D_query)
+                # merge W_q_cur to W_q
+                if W_q is None:
+                    W_q = W_q_cur * mask_cur[:,:,None]
+                else:
+                    # check repeat
+                    multi_projection = torch.logical_and(mask, mask_cur)
+                    if W_q_cur.size(2) > 1:
+                        reset = W_q_cur[:,:,0]*W_q_cur[:,:,1] * multi_projection > W_q[:,:,0]*W_q[:,:,1] * multi_projection
+                    else:
+                        reset = W_q_cur[:, :, 0] * multi_projection > W_q[:, :, 0] * multi_projection
+                    mask = mask & (~reset)
+                    mask_cur = mask_cur & ~(multi_projection & ~reset)
+
+                    W_q = W_q_cur * mask_cur[:,:,None] + W_q * mask[:,:,None]
+
+                if F_q is None:
+                    F_q = F_q_cur * mask_cur[:,:,None]
+                    mask = mask_cur
+                else:
+                    F_q = F_q_cur * mask_cur[:,:,None] + F_q * mask[:,:,None]
+                    mask = torch.logical_or(mask, mask_cur)
+
+            W_ref = pred['ref']['confidences'][i]
+            W_ref_q = (W_ref, W_q, confidence_count)
+
+            if self.conf.normalize_features:
+                F_q = nnF.normalize(F_q, dim=2)  # B x N x C
+                F_ref = nnF.normalize(F_ref, dim=1)  # B x C x W x H
+
+            T_opt, failed, T_opt_list = opt(dict(
+                p3D=p3D_query, F_ref=F_ref, F_q=F_q, T_init=T_init, camera=cam_ref,
+                mask=mask, W_ref_q=W_ref_q))
+
+            pred['T_q2r_init'].append(T_init)
+            pred['T_q2r_opt'].append(T_opt)
+            pred['T_q2r_opt_list'].append(T_opt_list)
+
+            T_init = T_opt.detach()
+
+            # add by shan, query & reprojection GT error, for query unet back propogate
+            if pose_loss:
+                loss_gt = self.preject_l1loss(opt, p3D_query, F_ref, F_q, data['T_q2r_gt'], cam_ref, mask=mask, W_ref_query=W_ref_q)
+                loss_init = self.preject_l1loss(opt, p3D_query, F_ref, F_q, data['T_q2r_init'], cam_ref, mask=mask, W_ref_query=W_ref_q)
+                diff_loss = torch.log(1 + torch.exp(10*(1- (loss_init + 1e-8) / (loss_gt + 1e-8))))
+                pred['pose_loss'].append(diff_loss)
+
+        return pred
+
+    def preject_l1loss(self, opt, p3D, F_ref, F_query, T_gt, camera, mask=None, W_ref_query= None):
+        args = (camera, p3D, F_ref, F_query, W_ref_query)
+        res, valid, w_unc, _, _ = opt.cost_fn.residuals(T_gt, *args)
+        if mask is not None:
+            valid &= mask
+
+        # compute the cost and aggregate the weights
+        cost = (res ** 2).sum(-1)
+        cost, w_loss, _ = opt.loss_fn(cost) # robust cost
+        loss = cost * valid.float()
+        if w_unc is not None:
+            loss = loss * w_unc
+
+        return torch.sum(loss, dim=-1)/(torch.sum(valid)+1e-6)
+
+    def loss(self, pred, data):
+        cam_ref = data['ref']['camera']
+        points_3d = pred['query']['grd_key_3d']
+        def project(T_q2r):
+            return cam_ref.world2image(T_q2r * points_3d)
+
+        p2D_r_gt, mask = project(data['T_q2r_gt'])
+        p2D_r_i, mask_i = project(data['T_q2r_init'])
+        mask = (mask & mask_i).float()
+
+        def reprojection_error(T_q2r):
+            p2D_r, _ = project(T_q2r)
+            err = torch.sum((p2D_r_gt - p2D_r)**2, dim=-1)
+            err = scaled_barron(1., 2.)(err)[0]/4
+            err = masked_mean(err, mask, -1)
+            return err
+
+        err_init = reprojection_error(pred['T_q2r_init'][0])
+
+        num_scales = len(self.extractor.scales)
+        success = None
+        losses = {'total': 0.}
+        if pose_loss:
+            losses['pose_loss'] = 0
+        for i, T_opt in enumerate(pred['T_q2r_opt']):
+            err = reprojection_error(T_opt).clamp(max=self.conf.clamp_error)
+            loss = err / num_scales
+            if i > 0:
+                loss = loss * success.float()
+            thresh = self.conf.success_thresh * self.extractor.scales[-1-i]
+            success = err < thresh
+            losses[f'reprojection_error/{i}'] = err
+            losses['total'] += loss
+
+            # add by shan, query & reprojection GT error, for query unet back propogate
+            if pose_loss:
+                losses['pose_loss'] += pred['pose_loss'][i]/ num_scales
+                # poss_loss_weight = 5
+                poss_loss_weight = get_weight_from_reproloss(err_init)
+                losses['total'] += (poss_loss_weight * pred['pose_loss'][i]/ num_scales).clamp(max=self.conf.clamp_error/num_scales)
+
+        losses['reprojection_error'] = err
+        losses['reprojection_error/init'] = err_init
+
+        return losses
+
+    # def metrics(self, pred, data):
+    #     T_r2q_gt = data['T_q2r_gt'].inv()
+    #
+    #     @torch.no_grad()
+    #     def scaled_pose_error(T_q2r):
+    #         err_R, err_t = (T_r2q_gt@T_q2r).magnitude()
+    #         err_lat, err_long = (T_r2q_gt@T_q2r).magnitude_latlong()
+    #         return err_R, err_t, err_lat, err_long
+    #
+    #     metrics = {}
+    #     for i, T_opt in enumerate(pred['T_q2r_opt']):
+    #         err = scaled_pose_error(T_opt)
+    #         metrics[f'R_error/{i}'], metrics[f't_error/{i}'], metrics[f'lat_error/{i}'], metrics[f'long_error/{i}'] = err
+    #     metrics['R_error'], metrics['t_error'], metrics['lat_error'], metrics[f'long_error']  = err
+    #
+    #     err_init = scaled_pose_error(pred['T_q2r_init'][0])
+    #     metrics['R_error/init'], metrics['t_error/init'], metrics['lat_error/init'], metrics[f'long_error/init'] = err_init
+    #
+    #     return metrics
+
+
+    def metrics(self, pred, data):
+        T_r2q_gt = data['T_q2r_gt'].inv()
+
+        @torch.no_grad()
+        def scaled_pose_error(T_q2r):
+            # err_R, err_t = (T_r2q_gt@T_q2r).magnitude()
+            # err_lat, err_long = (T_r2q_gt@T_q2r).magnitude_latlong()
+            err_R, err_t = (T_q2r @ T_r2q_gt).magnitude()
+            err_lat, err_long = (T_q2r @ T_r2q_gt).magnitude_latlong()
+            return err_R, err_t, err_lat, err_long
+
+        metrics = {}
+        for i, T_opt in enumerate(pred['T_q2r_opt']):
+            err = scaled_pose_error(T_opt)
+            metrics[f'R_error/{i}'], metrics[f't_error/{i}'], metrics[f'lat_error/{i}'], metrics[f'long_error/{i}'] = err
+        metrics['R_error'], metrics['t_error'], metrics['lat_error'], metrics[f'long_error']  = err
+
+        err_init = scaled_pose_error(pred['T_q2r_init'][0])
+        metrics['R_error/init'], metrics['t_error/init'], metrics['lat_error/init'], metrics[f'long_error/init'] = err_init
+
+        return metrics
+
+
+    def metrics_analysis(self, pred, data):
+        T_r2q_gt = data['T_q2r_gt'].inv()
+
+        @torch.no_grad()
+        def scaled_pose_error(T_q2r):
+            # err_R, err_t = (T_r2q_gt@T_q2r).magnitude()
+            # err_lat, err_long = (T_r2q_gt@T_q2r).magnitude_latlong()
+            err_R, err_t = (T_q2r @ T_r2q_gt).magnitude()
+            err_lat, err_long = (T_q2r @ T_r2q_gt).magnitude_latlong()
+            return err_R, err_t, err_lat, err_long
+
+        metrics = {}
+        # error init
+        err_init = scaled_pose_error(pred['T_q2r_init'][0])
+        metrics['R_error/init'], metrics['t_error/init'], metrics['lat_error/init'], metrics[
+            f'long_error/init'] = err_init
+
+        # error pred
+        pred['T_q2r_opt_list'] = list(itertools.chain(*pred['T_q2r_opt_list']))
+        R_error, t_error, lat_error, long_error = (torch.tensor([]).to(pred['T_q2r_init'][0].device),
+                                                  torch.tensor([]).to(pred['T_q2r_init'][0].device),
+                                                  torch.tensor([]).to(pred['T_q2r_init'][0].device),
+                                                  torch.tensor([]).to(pred['T_q2r_init'][0].device))
+
+
+        for j, T_opt in enumerate(pred['T_q2r_opt_list']):
+            err = scaled_pose_error(T_opt)
+            # R_error, t_error, lat_error, lon_error = err
+            R_error = torch.cat([R_error, err[0]])
+            t_error = torch.cat([t_error, err[1]])
+            lat_error = torch.cat([lat_error, err[2]])
+            long_error = torch.cat([long_error, err[3]])
+
+        metrics['R_error'] = R_error
+        metrics['t_error'] = t_error
+        metrics['lat_error'] = lat_error
+        metrics['long_error'] = long_error
+
+        return metrics
---
+++ PureACL/pixlib/models/unet_debug.py
@@ -0,0 +1,276 @@
+"""
+Flexible UNet model which takes any Torchvision backbone as encoder.
+Predicts multi-level feature and uncertainty maps
+and makes sure that they are well aligned.
+"""
+
+import torchvision
+import torch
+import torch.nn as nn
+
+from .base_model import BaseModel
+from .utils import checkpointed
+from PureACL.pixlib.models.utils import camera_to_onground
+
+from PureACL.pixlib.geometry.interpolation import interpolate_tensor
+from PureACL.visualization.viz_2d import imsave
+
+# for 1 unet test
+two_confidence = True # False when only grd
+max_dis = 200
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, previous, skip, out, num_convs=1, norm=nn.BatchNorm2d):
+        super().__init__()
+
+        self.upsample = nn.Upsample(
+                scale_factor=2, mode='bilinear', align_corners=False)
+
+        layers = []
+        for i in range(num_convs):
+            conv = nn.Conv2d(
+                previous+skip if i == 0 else out, out,
+                kernel_size=3, padding=1, bias=norm is None)
+            layers.append(conv)
+            if norm is not None:
+                layers.append(norm(out))
+            layers.append(nn.ReLU(inplace=True))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, previous, skip):
+        upsampled = self.upsample(previous)
+        # If the shape of the input map `skip` is not a multiple of 2,
+        # it will not match the shape of the upsampled map `upsampled`.
+        # If the downsampling uses ceil_mode=False, we nedd to crop `skip`.
+        # If it uses ceil_mode=True (not supported here), we should pad it.
+        _, _, hu, wu = upsampled.shape
+        _, _, hs, ws = skip.shape
+        assert (hu <= hs) and (wu <= ws), 'Using ceil_mode=True in pooling?'
+        # assert (hu == hs) and (wu == ws), 'Careful about padding'
+        skip = skip[:, :, :hu, :wu]
+
+        return self.layers(torch.cat([upsampled, skip], dim=1))
+
+class AdaptationBlock(nn.Sequential):
+    def __init__(self, inp, out):
+        conv = nn.Conv2d(inp, out, kernel_size=1, padding=0, bias=True)
+        super().__init__(conv)
+
+
+class UNet(BaseModel):
+    default_conf = {
+        'output_scales': [0, 2, 4],  # what scales to adapt and output
+        'output_dim': [32, 128, 128],  # # of channels in output feature maps
+        'encoder': 'vgg16',  # string (torchvision net) or list of channels
+        'num_downsample': 4,  # how many downsample block (if VGG-style net)
+        'decoder': [64, 64, 64, 32],  # list of channels of decoder
+        'decoder_norm': 'nn.BatchNorm2d',  # normalization ind decoder blocks
+        'do_average_pooling': False,
+        'compute_uncertainty': True,
+        'checkpointed': False,  # whether to use gradient checkpointing
+    }
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
+    def build_encoder(self, conf):
+        assert isinstance(conf.encoder, str)
+        Encoder = getattr(torchvision.models, conf.encoder)
+        # encoder = Encoder(pretrained=True)
+        encoder = Encoder(pretrained=False)
+        encoder.load_state_dict(torch.load('/ws/external/checkpoints/pretrained/vgg16-397923af.pth'))
+        Block = checkpointed(torch.nn.Sequential, do=conf.checkpointed)
+
+        if conf.encoder.startswith('vgg'):
+            # Parse the layers and pack them into downsampling blocks
+            # It's easy for VGG-style nets because of their linear structure.
+            # This does not handle strided convs and residual connections
+            assert max(conf.output_scales) <= conf.num_downsample
+            skip_dims = []
+            previous_dim = None
+            blocks = [[]]
+            for i, layer in enumerate(encoder.features):
+                if isinstance(layer, torch.nn.Conv2d):
+                    previous_dim = layer.out_channels
+                elif isinstance(layer, torch.nn.MaxPool2d):
+                    assert previous_dim is not None
+                    skip_dims.append(previous_dim)
+                    if (conf.num_downsample + 1) == len(blocks):
+                        break
+                    blocks.append([])
+                    if conf.do_average_pooling:
+                        assert layer.dilation == 1
+                        layer = torch.nn.AvgPool2d(
+                            kernel_size=layer.kernel_size, stride=layer.stride,
+                            padding=layer.padding, ceil_mode=layer.ceil_mode,
+                            count_include_pad=False)
+                blocks[-1].append(layer)
+            assert (conf.num_downsample + 1) == len(blocks)
+            encoder = [Block(*b) for b in blocks]
+        elif conf.encoder.startswith('resnet'):
+            # Manually define the splits - this could be improved
+            assert conf.encoder[len('resnet'):] in ['18', '34', '50', '101']
+            block1 = torch.nn.Sequential(encoder.conv1, encoder.bn1,
+                                         encoder.relu)
+            block2 = torch.nn.Sequential(encoder.maxpool, encoder.layer1)
+            block3 = encoder.layer2
+            block4 = encoder.layer3
+            blocks = [block1, block2, block3, block4]
+            encoder = [torch.nn.Identity()] + [Block(b) for b in blocks]
+            skip_dims = [3, 64, 256, 512, 1024]
+        else:
+            raise NotImplementedError(conf.encoder)
+
+        encoder = nn.ModuleList(encoder)
+        return encoder, skip_dims
+
+    def _init(self, conf):
+        # Encoder
+        self.encoder, skip_dims = self.build_encoder(conf)
+        self.add_extra_input()  # add for pose enbedding
+
+        # Decoder
+        if conf.decoder is not None:
+            assert len(conf.decoder) == (len(skip_dims) - 1)
+            Block = checkpointed(DecoderBlock, do=conf.checkpointed)
+            norm = eval(conf.decoder_norm) if conf.decoder_norm else None
+
+            previous = skip_dims[-1]
+            decoder = []
+            for out, skip in zip(conf.decoder, skip_dims[:-1][::-1]):
+                decoder.append(Block(previous, skip, out, norm=norm))
+                previous = out
+            self.decoder = nn.ModuleList(decoder)
+
+        # Adaptation layers
+        adaptation = []
+        if conf.compute_uncertainty:
+            uncertainty = []
+        for idx, i in enumerate(conf.output_scales):
+            if conf.decoder is None or i == (len(self.encoder) - 1):
+                input_ = skip_dims[i]
+            else:
+                input_ = conf.decoder[-1-i]
+
+            # out_dim can be an int (same for all scales) or a list (per scale)
+            dim = conf.output_dim
+            if not isinstance(dim, int):
+                dim = dim[idx]
+
+            block = AdaptationBlock(input_, dim)
+            adaptation.append(block)
+            if conf.compute_uncertainty:
+                if two_confidence:
+                    uncertainty.append(AdaptationBlock(input_, 2))
+                else:
+                    uncertainty.append(AdaptationBlock(input_, 1))
+        self.adaptation = nn.ModuleList(adaptation)
+
+        self.scales = [2**s for s in conf.output_scales]
+        if conf.compute_uncertainty:
+            self.uncertainty = nn.ModuleList(uncertainty)
+
+    def _forward(self, data):
+        image = data['image']
+        # mean, std = image.new_tensor(self.mean), image.new_tensor(self.std)
+        # image = (image - mean[:, None, None]) / std[:, None, None]
+
+        # embedding height & distance & angle
+        b, _, h, w = image.shape
+        device = image.device
+        vv, uu = torch.meshgrid(torch.arange(h, device=device), torch.arange(w, device=device), indexing='ij')
+        uv = torch.stack([uu, vv], dim=-1)
+        uv = uv[None, :, :, :].repeat(b, 1, 1, 1)  # shape = [b, h, w, 2]
+        p3d_c = data['camera'].image2world(uv)  # [b, h, w, 3]
+        if data['type'] == 'grd':
+            p3d_q = torch.einsum('bij,bhwj->...bhwi', data['T_w2cam'].inv().R, p3d_c)  # query world coordinate
+            height = p3d_q[..., 2]
+            angle = p3d_q[..., 0] / torch.sqrt(p3d_q[..., 0] ** 2 + p3d_q[..., 1] ** 2)  # cos -1~1
+            p3d_ongrd_q = camera_to_onground(p3d_c, data['T_w2cam'], data['camera_h'], data['normal'])
+            dis = torch.sqrt((p3d_ongrd_q[..., 0]) ** 2 + (p3d_ongrd_q[..., 1]) ** 2) / max_dis
+            dis = torch.where(dis > 1.2, torch.tensor(-1.).to(dis), dis)
+            if 1:
+                p3d_ongrd_q = data['q2r_gt'] * p3d_ongrd_q.reshape(-1, h * w, 3)
+
+                p2d_ref, ref_mask = data['ref']['camera'].world2image2(
+                    data['ref']['T_w2cam'] * p3d_ongrd_q.reshape(-1, h * w, 3))
+                # p2d_ref = p2d_ref.reshape(-1, h*w, 2)
+                r2q_img, r2q_mask, _ = interpolate_tensor(data['ref']['image'], p2d_ref, 'linear',
+                                                          pad=4, return_gradients=False, out_shape='bcn')
+                b, c, h, w = data['image'].shape
+                r2q_img, r2q_mask = r2q_img.reshape(-1, c, h, w), r2q_mask.reshape(-1, 1, h, w)
+                # debugging
+                imsave(r2q_img[0], '/ws/external/visualizations/dense2', '1r2q')
+                imsave(data['image'][0], '/ws/external/visualizations/dense2', '1grd')
+                imsave(data['ref']['image'][0], '/ws/external/visualizations/dense2', '1sat')
+
+        else:
+            p3d_q = torch.einsum('bij,bhwj->...bhwi', data['q2r'].inv().R, p3d_c)  # query world coordinate
+            angle = p3d_q[..., 0] / torch.sqrt(p3d_q[..., 0] ** 2 + p3d_q[..., 1] ** 2)  # cos -1~1
+            dis = torch.sqrt(p3d_q[..., 0] ** 2 + p3d_q[..., 1] ** 2)/max_dis
+            height = -0.6 * torch.ones_like(p3d_q[..., 2]) # all -0.6 as max height
+
+            if 1:
+                # p3d_q = torch.einsum('bij,bhwj->...bhwi', data['q2r_gt'].inv().R, p3d_c)  # query world coordinate
+                p3d_q = data['q2r_gt'].inv() * p3d_c.reshape(-1, h * w, 3)
+                p2d_q, mask = data['query']['camera'].world2image2(
+                    data['query']['T_w2cam'] * p3d_q.reshape(-1, h * w, 3))
+                # p2d_q = p2d_q.reshape(-1, h*w, 2)
+                q2r_img, q2r_mask, _ = interpolate_tensor(data['query']['image'], p2d_q,
+                                                          'linear', pad=4, return_gradients=False,
+                                                          out_shape='bcn')
+                b, c, h, w = data['image'].shape
+                q2r_img, q2r_mask = q2r_img.reshape(-1, c, h, w), q2r_mask.reshape(-1, 1, h, w)
+                imsave(q2r_img[0], '/ws/external/visualizations/dense2', '0q2r')
+                imsave(data['image'][0], '/ws/external/visualizations/dense2', '0sat')
+                imsave(data['query']['image'][0], '/ws/external/visualizations/dense2', '0grd')
+
+        extr = torch.stack([angle, height, dis], dim=1).to(device)  # shape = [b, 3, h, w]
+        image = torch.cat([image, extr], dim=1) # shape = [b, 6, h, w]
+
+        skip_features = []
+        features = image
+        for block in self.encoder:
+            features = block(features)
+            skip_features.append(features)
+
+
+        if self.conf.decoder:
+            pre_features = [skip_features[-1]]
+            for block, skip in zip(self.decoder, skip_features[:-1][::-1]):
+                pre_features.append(block(pre_features[-1], skip))
+            pre_features = pre_features[::-1]  # fine to coarse
+        else:
+            pre_features = skip_features
+
+        out_features = []
+        for adapt, i in zip(self.adaptation, self.conf.output_scales):
+            out_features.append(adapt(pre_features[i]))
+        pred = {'feature_maps': out_features}
+
+        if self.conf.compute_uncertainty:
+            confidences = []
+            for layer, i in zip(self.uncertainty, self.conf.output_scales):
+                unc = layer(pre_features[i])
+                conf = torch.sigmoid(-unc)
+                confidences.append(conf)
+            pred['confidences'] = confidences
+
+        return pred
+
+    def loss(self, pred, data):
+        raise NotImplementedError
+
+    def metrics(self, pred, data):
+        raise NotImplementedError
+
+    def add_extra_input(self):
+        layer = self.encoder[0][0]
+        # Creating new Conv2d layer
+        new_layer = nn.Conv2d(6,64,kernel_size=3,padding=1).to(layer.weight)
+        new_weight = torch.cat([layer.weight.clone(), new_layer.weight[:,3:].clone()], dim=1)
+        new_layer.weight = nn.Parameter(new_weight)
+        self.encoder[0][0] = new_layer
+
+
+
